d6f41cd3795b30080d37986ee95f7b38ad2af4b0
marcelomuotri <75761575+marcelomuotri@users.noreply.github.com>
2021-10-09 13:58:41 +0200

    arreglos centrados

--- src/components/quienesSomos/QuienesSomos.jsx
+++ src/components/quienesSomos/QuienesSomos.jsx
@@ -1,7 +1,7 @@
 import lineaAzul from '../../assets/azul/Decore.png'
 import Calesita from './carrousel/Carrousel'
 import { DataContext } from '../context/DataProvider'
-import React, {useState, useContext} from 'react'
+import React, { useState, useContext } from 'react'
 
 const QuienesSomos = () => {
 
@@ -18,13 +18,13 @@ const QuienesSomos = () => {
             <div className="sobreNosotros__texto">
                 <p className="sobreNosotros__cuerpo"> {idioma[0].textoSobre1}
 
-                    
+
                 </p>
                 <p className="sobreNosotros__cuerpo">  {idioma[0].textoSobre2}</p>
             </div>
-
-            <Calesita/>
-
+            <div className="sobreNosotros__contenedor" >
+                <Calesita />
+            </div>
         </div>
     )
 }
